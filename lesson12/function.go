4bbd5cde5371c7fb6bee5f6c10b896f924418fa2
Kazuhiro Miyahara <kazuhiro.miyahara.vs@gmail.com>
2019-10-17 13:08:01 +0000

    lesson12

---
+++ lesson12/function.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func celsiusToFahrenheit(c float64) float64 {
+	return (c * 9.0 / 5.0) + 32.0
+
+}
+
+func kelvinToFahrenheit(k float64) float64 {
+	return celsiusToFahrenheit(k - 273.15)
+}
+
+func main() {
+	fmt.Println(celsiusToFahrenheit(0.0))
+	fmt.Println(kelvinToFahrenheit(0.0))
+}
