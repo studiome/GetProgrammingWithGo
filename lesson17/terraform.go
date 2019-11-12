e1580f6a1d00c3007fa56633ca795de561b56a9a
Kazuhiro Miyahara <kazuhiro.miyahara.vs@gmail.com>
2019-11-12 05:41:04 +0000

    slice is a pointer

--- lesson17/terraform.go
+++ lesson17/terraform.go
@@ -16,6 +16,7 @@ func (p Planets) terraform() Planets {
 
 func main() {
 	planets := []string{"Mars", "Uranus", "Neptune"}
+	fmt.Println(planets)
 	fmt.Println(Planets(planets).terraform())
 	fmt.Println(planets)
 }
